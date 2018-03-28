4f2aa24e1f405d8d0b17ed70fb8cc73ec298c95c
god_of_sound <robbe.baeyens@outlook.com>
2018-03-28 10:02:13 +0200

    send mail

--- contact.php
+++ contact.php
@@ -78,6 +78,27 @@
                     if (isset($_POST["feedback"]) && $_POST["feedback"] != "") {
                     			echo '<li>Opmerking:</li><div class="tekst">' . $_POST["feedback"] . '</div>';
                     }
+                    // Check for empty fields
+                    if(empty($_POST['gebruikersnaam'])      ||
+                       empty($_POST['email'])     ||
+                       !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+                       {
+                       echo "No arguments Provided!";
+                       return false;
+                       }
+
+                    $name = strip_tags(htmlspecialchars($_POST['gebruikersnaam']));
+                    $email_address = strip_tags(htmlspecialchars($_POST['email']));
+                    $message = strip_tags(htmlspecialchars($_POST['feedback']));
+
+                    // Create the email and send the message
+                    $to = 'robbe.baeyens@outlook.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+                    $email_subject = "Website Contact Form:  $name";
+                    $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+                    $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+                    $headers .= "Reply-To: $email_address";
+                    mail($to,$email_subject,$email_body,$headers);
+                    return true;
                 ?>
             </ul>
         </div>
